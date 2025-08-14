7247cee0c22d9f6aff0830e80e2c026f349d3347
EightShin <trickxter15@gmail.com>
2025-08-14 19:20:01 +0800

    Updaated-Final

--- Fernandez_Carledjay/SavingsAccount.java
+++ Fernandez_Carledjay/SavingsAccount.java
@@ -1,21 +1,27 @@
 public class SavingsAccount {
-    private double balance;
-    public static double interestRate = 0;
+    private double balance;                
+    private static double interestRate = 0;
+
     public SavingsAccount() {
         balance = 0;
     }
+
     public static void setInterestRate(double newRate) {
         interestRate = newRate;
     }
+
     public static double getInterestRate() {
         return interestRate;
     }
+
     public double getBalance() {
         return balance;
     }
+
     public void deposit(double amount) {
         balance += amount;
     }
+
     public double withdraw(double amount) {
         if (balance >= amount) {
             balance -= amount;
@@ -24,11 +30,13 @@ public class SavingsAccount {
             return 0;
         }
     }
+
     public void addInterest() {
         double interest = balance * interestRate;
         balance += interest;
     }
-    public static void showBalance(SavingsAccount account) {
-        System.out.println("Current balance: " + account.getBalance());
+
+    public void showBalance() {
+        System.out.println("Current balance: " + balance);
     }
 }
