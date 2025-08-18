b4fa52459b0bbc3c42d808c9f6e15032d2364860
EightShin <trickxter15@gmail.com>
2025-08-18 23:41:52 +0800

    Final3.0

--- AmountDue.java
+++ AmountDue.java
@@ -1,11 +1,11 @@
 class AmountDue {
 
     public double computeAmountDue(double price) {
-        return price * 1.12;
+        return price + price * 0.12;
     }
 
     public double computeAmountDue(double price, double quantity) {
-        return price * quantity * 1.12;
+        return price * quantity + price * quantity * 0.12;
     }
 
     public double computeAmountDue(double price, double quantity, double discount) {
